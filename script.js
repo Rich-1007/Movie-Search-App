f082e878bca2a8bc372f03c48ba85a7d95b75a48
Hricheak Ghosh <148091835+Rich-1007@users.noreply.github.com>
2023-12-26 13:30:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,45 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-let main = document.querySelector(".main-11");
+let main = document.querySelector(".main");
 
 async function getMovies(api) {
   let res = await fetch(api);
   let data = await res.json();
 
-  showMovies(data.results);
+  showItems(data.results);
 }
 
-async function showMovies(dd) {
+async function showItems(data) {
   main.innerHTML = "";
 
-  dd.forEach((item) => {
-    let newdiv = document.createElement("div");
+  data.forEach((item) => {
+    // console.log(item);
 
-    newdiv.classList.add = "papa";
-    newdiv.innerHTML = `
-      <div class = "aa">
-      
-           <div class = "tt">
-                 <img src="${IMGPATH + item.poster_path}">      
-                     
-                 <div class = "mm">
-                    <p>${item.original_title}</p>
-                    <p>${item.vote_average}</p>
-                    
-                 </div>
-                 
-                 <div class = "qq">
-                 
-                    <p>${item.overview} </p>
-                 </div>
-           </div>
-      
-      
-      
-      </div>
+    let outerdiv = document.createElement("div");
+    outerdiv.innerHTML = `
+                      <div class="card">
 
-      `;
-    main.appendChild(newdiv);
+                          <div class="img-div  ">
+                                <img src="${IMGPATH + item.poster_path}"  alt="Image Not Found"/>
+                          </div>
+  
+                          <div class="title-rating">
+                                <p>${item.original_title}</p>
+                                <p>${item.vote_average}</p>
+                          </div>
+  
+                           <div class="overview">
+                                <p>${item.overview}</p>
+                           </div>
+                                
+                      </div>;`;
+
+    main.appendChild(outerdiv);
   });
 }
 
-document.querySelector("#search").addEventListener("keyup", function (event) {
+document.querySelector("input").addEventListener("keyup", function (event) {
   if (event.target.value != "") {
     getMovies(SEARCHAPI + event.target.value);
   } else {
